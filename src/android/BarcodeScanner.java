da0fbfe338b9094b03f428c9f532140590557523
Ivan <iwalulya@yahoo.co.uk>
2019-10-02 10:12:34 +0200

    working on gradle

--- src/android/BarcodeScanner.java
+++ src/android/BarcodeScanner.java
@@ -63,6 +63,8 @@ public class BarcodeScanner extends CordovaPlugin implements BarCodeReader.Decod
     private String decodeStatString;
     private static int decCount = 0;
     
+    /*
+    
     static {
         System.loadLibrary("IAL");
         System.loadLibrary("SDL");
@@ -80,7 +82,7 @@ public class BarcodeScanner extends CordovaPlugin implements BarCodeReader.Decod
 
         // System.loadLibrary("barcodereader"); // Android 2.3 - Android 4.2
         
-    } 
+    }  */
 
     
     @Override
